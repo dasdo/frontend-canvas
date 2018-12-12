d53f23c18301751724390945701ca1bc81fb6d19
Enmanuel Pujals <enmanuel@mctekk.com>
2018-12-12 11:48:01 -0400

    Pruebas canvas Kaury

---
+++ src/views/testb/boxblue.vue
@@ -0,0 +1,29 @@
+<template>
+     <div class="container-fluid card card-default">
+        <!-- Title box -->
+        <div class="row">
+        <div class="col text-center">
+            <h4>Colum 1</h4>
+        </div>
+        <div class="col text-center">
+            <h4>Colum 2</h4>
+        </div>
+        </div>
+
+        <div class="row">
+            <div class="col text-center">
+                <p>Titulo 1</p>
+                <button type="button" class="btn btn-warning btn-lg">
+                    Notificaciones <span class="badge badge-lignt">8</span>
+                </button>
+            </div>
+            <div class="col text-center">
+                <p>Titulo 2</p>
+                <button type="button" class="btn btn-success btn-lg">
+                    Notificaciones <span class="badge badge-lignt">8</span>
+                </button>
+
+            </div>
+        </div>
+    </div>
+</template>
---
+++ src/views/testb/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="testB">
+
+    <boxblue-component></boxblue-component>
+    <boxyellow-component></boxyellow-component>
+
+  </div>
+</template>
+
+<script>
+import BoxblueComponent from './boxblue';
+import BoxyellowComponent from './boxyellow';
+export default {
+    components: {
+         BoxblueComponent,
+         BoxyellowComponent,
+    }
+};
+
+</script>
+
+<style lang="scss" scoped>
+.testB{
+    margin:20px;
+
+    .row{
+        padding: 10px;
+        text-align: center;
+    }
+}
+</style>
